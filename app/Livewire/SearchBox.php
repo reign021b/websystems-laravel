15857560be3ace2bd1c05830305326e8ffcf68b9
ryoong <reign021b@gmail.com>
2023-12-26 01:15:20 +0800

    First commit

---
+++ app/Livewire/SearchBox.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
---
+++ resources/views/components/input.blade.php
@@ -0,0 +1,4 @@
+@props(['disabled' => false])
+
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' =>
+'rounded bg-gray-100 focus:outline-none focus:border-none focus:ring-0 outline-none border-none text-xs text-gray-800 placeholder:tex']) !!}>
